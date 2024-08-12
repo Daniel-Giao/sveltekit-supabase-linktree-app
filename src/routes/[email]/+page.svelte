7a6fc2c51795b1c9037a4ade6a90e07f5f6f2a1d
Daniel Giao <daniel.giao@icloud.com>
2024-08-11 22:42:44 -0400

    Added modal for profile edits

--- src/routes/[email]/+page.svelte
+++ src/routes/[email]/+page.svelte
@@ -14,6 +14,27 @@
     description: "Cool description",
     pokemon_ids: [1, 2, 3],
   };
+  let isModalOpen = false;
+
+  async function saveProfile() {
+    const { data: profileData, error: profileError } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("email", email);
+
+    if (profileData?.length === 0) {
+      const { data, error } = await supabase.from("profiles").insert({
+        ...profile,
+        user_id: session?.user?.id,
+        email: session?.user?.email,
+      });
+    } else {
+      const { data, error } = await supabase
+        .from("profiles")
+        .update(profile)
+        .eq("user_id", session?.user?.id);
+    }
+  }
 
   async function refreshPokemonData() {
     pokemonData = [];
@@ -24,18 +45,56 @@
   }
   page.subscribe(async () => {
     pokemonList = await getPokemonList();
+
+    const { data: profileData, error: profileError } = await supabase
+      .from("profiles")
+      .select("description, pokemon_ids")
+      .eq("email", email);
+
+    if (profileData?.length === 0 && email === session?.user?.email) {
+      console.log("SAVE PROFILE");
+      await saveProfile();
+    } else if (profileData !== null && profileData.length > 0) {
+      profile = profileData[0];
+    } else {
+      console.log("NO PROFILE");
+      profile = {
+        description: "This user does not have a profile yet",
+        pokemon_ids: [],
+      };
+    }
+
     await refreshPokemonData();
-    console.log(await getPokemon("pikachu"));
-    console.log(pokemonList);
-    console.log(pokemonData);
   });
+
+  async function savePageEdits() {
+    console.log("Saving edits");
+    isModalOpen = false;
+  }
+
+  async function togglePokemon(id: number) {
+    let pokemonIDs = profile.pokemon_ids;
+
+    if (pokemonIDs.length >= 3 && !pokemonIDs.includes(id)) {
+      alert("You can only have 3 pokemon maximum!");
+      return;
+    }
+    if (pokemonIDs.includes(id)) {
+      let index = pokemonIDs.indexOf(id);
+      pokemonIDs.splice(index, 1);
+    } else {
+      pokemonIDs.push(id);
+    }
+
+    profile.pokemon_ids = [...pokemonIDs];
+  }
 </script>
 
 <div class="hero min-h-screen bg-base-300">
   <div class="hero-content">
     <div class="max-w-2xl text-center">
       <h1 class="font-bold text-4xl">{email}'s Page</h1>
-      <p class="py-3 max-w-md mx-auto">User description here...</p>
+      <p class="py-3 max-w-md mx-auto">{profile.description}</p>
       <div class="grid grid-cols-3">
         {#if !pokemonData}
           <p>Loading...</p>
@@ -57,6 +116,44 @@
           {/each}
         {/if}
       </div>
+      {#if email === session?.user?.email}
+        <button class="btn btn-info" on:click={() => (isModalOpen = true)}>
+          Edit Page
+        </button>
+        <dialog class="modal min-w-lg" class:modal-open={isModalOpen}>
+          <div class="modal-box">
+            <h3>Edit your PokePage</h3>
+            <p>
+              Here you can make edits to your page, such as description or
+              pokemon selected
+            </p>
+            <p class="p-2">Edit your description</p>
+            <textarea
+              bind:value={profile.description}
+              class="textarea textarea-bordered textarea-lg w-full max-w-md h-[300px]"
+            />
+            <p class="p-2">Select your pokemon</p>
+            <div class="grid grid-cols-3 overflow-y-scroll max-h-[600px] m-3">
+              {#each pokemonList as pokemon, index}
+                <button
+                  class={"card bg-slate-700 h-12 p-1 m-1 justify-center items-center " +
+                    (profile.pokemon_ids.includes(index + 1)
+                      ? "border-2 border-blue-600"
+                      : "")}
+                  on:click={() => togglePokemon(index + 1)}
+                >
+                  <div class="text-center">
+                    <h2 class="text-white text-xl">{pokemon.name}</h2>
+                  </div>
+                </button>
+              {/each}
+            </div>
+            <button class="btn btn-success" on:click={() => savePageEdits()}>
+              Save Edits
+            </button>
+          </div>
+        </dialog>
+      {/if}
     </div>
   </div>
 </div>
